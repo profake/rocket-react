62423ac71264c24b3fb2084a7267d9ba1720523b
profake <nasifistiak@gmail.com>
2022-05-07 17:30:36 +0600

    added basic api calls

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { useEffect } from "react";
+import { fetchRocketLaunchData } from "./store/AppStore";
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
-  return (
-    <div>
-      
-    </div>
-  );
+  const dispatch = useDispatch();
+  const data = useSelector((state) => state.rocket.rockets);
+  
+  useEffect(() => {
+    dispatch(fetchRocketLaunchData());
+    
+  }, [dispatch]);
+
+  return data.map((item) => {
+    return <p>{item.name}</p>;
+  });
 }
 
 export default App;
--- src/store/AppStore.js
+++ src/store/AppStore.js
@@ -1,7 +1,53 @@
-import { createStore } from "redux";
+import { createSlice, configureStore } from "@reduxjs/toolkit";
 
-const rocketReducer = (state = {}, action) => {}
+const initialState = {
+  rockets: [{ name: "meme" }],
+};
 
-const store = createStore();
+const rocketSlice = createSlice({
+  name: "rocketData",
+  initialState,
+  reducers: {
+    replaceRockets(state, action) {
+      state.rockets = action.payload.rockets;
+      console.log("hiiii");
+    },
+  },
+});
 
+export const fetchRocketLaunchData = () => {
+  return async (dispatch) => {
+    const fetchData = async () => {
+      const response = await fetch("https://api.spacexdata.com/v3/launches/");
+
+      if (!response.ok) {
+        throw new Error("Error fetching data");
+      }
+
+      const data = await response.json();
+      return data;
+    };
+
+    try {
+      const rocketLaunchFetchedData = await fetchData();
+      const rockets = [];
+
+      rocketLaunchFetchedData.forEach((arrayItem) => {
+        // console.log(arrayItem.rocket.rocket_name)
+        rockets.push({ name: arrayItem.rocket.rocket_name });
+      });
+
+      dispatch(rocketActions.replaceRockets({ rockets: rockets }));
+      console.log(rocketSlice.rockets);
+    } catch (error) {
+      console.log("ERROR: " + error);
+    }
+  };
+};
+
+const store = configureStore({
+  reducer: { rocket: rocketSlice.reducer },
+});
+
+export const rocketActions = rocketSlice.actions;
 export default store;
