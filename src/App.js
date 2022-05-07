2e39113589fed7d500b51c37cfe3047832641bc7
profake <nasifistiak@gmail.com>
2022-05-07 20:39:35 +0600

    changed design of cards and changed font to open sans

--- src/App.js
+++ src/App.js
@@ -2,36 +2,67 @@ import { useEffect, useState } from "react";
 import { fetchRocketLaunchData } from "./store/AppStore";
 import { useDispatch, useSelector } from "react-redux";
 import Pagination from "@mui/material/Pagination";
+import Box from "@mui/material/Box";
 import Card from "./components/Card";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { Container, CssBaseline, List, ListItem } from "@mui/material";
+import '@fontsource/open-sans/300.css'
+import '@fontsource/open-sans/400.css'
+import '@fontsource/open-sans/500.css'
+import '@fontsource/open-sans/700.css'
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+  typography: {
+      fontFamily: "'Open Sans', sans-serif",
+      textTransform: 'none',
+  }
+});
 
 function App() {
   const dispatch = useDispatch();
   const data = useSelector((state) => state.rocket.rockets);
   const [pageNumber, setPageNumber] = useState();
 
-  const paginationChangeHandler = (event, page) => {setPageNumber(page); console.log(page)};
+  const paginationChangeHandler = (event, page) => {
+    setPageNumber(page);
+    console.log(page);
+  };
 
   useEffect(() => {
     dispatch(fetchRocketLaunchData(pageNumber));
   }, [dispatch, pageNumber]);
 
   return (
-    <>
-      <Pagination count={10} onChange={paginationChangeHandler}/>
-      <ul>
-        {data.map((item) => {
-          return (
-            <li>
-              <Card
-                mission_name={item.mission_name}
-                img_url={item.img_url}
-                rocket_name={item.rocket_name}
-              ></Card>
-            </li>
-          );
-        })}
-      </ul>
-    </>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Container >
+        <Box display="flex" justifyContent="center" alignItems="center">
+          <List >
+            {data.map((item) => {
+              return (
+                <ListItem>
+                  <Card
+                    mission_name={item.mission_name}
+                    img_url={item.img_url}
+                    rocket_name={item.rocket_name}
+                  ></Card>
+                </ListItem>
+              );
+            })}
+          </List>
+        </Box>
+        <Box my={2} display="flex" justifyContent="center">
+          <Pagination
+            count={10}
+            size="small"
+            onChange={paginationChangeHandler}
+          />
+        </Box>
+      </Container>
+    </ThemeProvider>
   );
 }
 
